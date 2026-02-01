abac4b891646d9488b14b75616dc4a7101d246a0
Saif-Mansour-50 <saif200716@gmail.com>
2026-02-01 12:23:18 +0200

    intro contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
+import { Notfound } from './components/notfound/notfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'contact', component: Contact, title: 'blog' },
+  { path: 'about', component: About, title: 'about' },
+  { path: '**', component: Notfound },
+];
