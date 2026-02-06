0fadba2d46f7d0d20865f89e23f6ae3eae5c5994
Saif-Mansour-50 <saif200716@gmail.com>
2026-02-06 10:28:03 +0200

    style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './components/home/home';
 import { About } from './components/about/about';
 import { Contact } from './components/contact/contact';
 import { Notfound } from './components/notfound/notfound';
+import { Details } from './components/details/details';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'contact', component: Contact, title: 'blog' },
   { path: 'about', component: About, title: 'about' },
+  { path: 'details/:slug', component: Details, title: 'details' },
   { path: '**', component: Notfound },
 ];
